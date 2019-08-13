eabb3fbd19df36e4f7ee4cee9ccbcafb85583d95
Stefan Wawrzyn <swawrzyn@gmail.com>
2019-08-13 02:45:23 +0000

    mostly finished just need translation.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js
+  //app.js
 App({
   onLaunch: function () {
     wx.BaaS = requirePlugin('sdkPlugin');
@@ -8,8 +8,29 @@ App({
       wx.requestPayment)
 
     wx.BaaS.init('4a2a0112bcf7cefccb94');
+
+    wx.BaaS.auth.loginWithWechat().then(user => {
+      console.log('user', user.id);
+      let query = new wx.BaaS.Query();
+      query.compare('created_by', '=', user.id);
+      let Rsvp = new wx.BaaS.TableObject('rsvp');
+      Rsvp.setQuery(query).find().then(res => {
+        console.log('res', res.data.objects);
+        if (res.data.objects.length > 0) {
+          this.globalData.sentRSVP = true;
+        } else {
+          this.globalData.sentRSVP = false;
+        }
+      }, err => {
+        // err
+      })
+    }, err => {
+      // 登录失败
+    })
   },
+  
   globalData: {
-    userInfo: null
+    userInfo: null,
+    sentRSVP: null,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+const app = getApp();
+
 const order = ['one', 'two', 'three'];
 
 const texts = {
@@ -10,11 +12,26 @@ const texts = {
     date: 'Saturday, September 28th, 2019',
     time: "6 o'clock in the evening",
     locationName: 'The Riveira Restaurant',
-    address: '505 Zhongshan Dong Er Road, Huangpu Dist., Shanghai',
+    address1: '505 Zhongshan Dong Er Road,',
+    address2: 'Huangpu District, Shanghai',
     detailsTitle: 'Details',
     detailsPara: "We would like to invite you for a small dinner at the Rivera Restaurant to celebrate our wedding. It's an occation for family and friends to come together and have a few laughs, some drinks and good food.",
     directions: 'Directions',
     rsvpTitle: 'RSVP',
+    rsvpSubtitle: 'Please fill in the form below',
+    rsvpYourName: 'What is your name?',
+    rsvpQuestion: 'Will you be attending?',
+    rsvpYes: 'Yes',
+    rsvpNo: 'No',
+    rsvpGuestRadio: 'Will you be bringing someone?',
+    rsvpGuestName: 'What is their name?',
+    rsvpSubmit: 'Submit',
+    mainNameError: 'Please enter your name',
+    radioError: 'Please select an option',
+    guestNameError: "Please enter your guest's name",
+    rsvpSent1: 'Congrats!',
+    rsvpSent2: 'Your RSVP has been sent successfully!',
+    rsvpSent3: 'See you soon!',
   },
   zh: {
     topText: 'together with their families',
@@ -25,10 +42,27 @@ const texts = {
     date: 'Saturday, September 28th, 2019',
     time: "6 o'clock in the evening",
     locationName: 'The Riveira Restaurant',
-    address: '505 Zhong Shan Dong Er Road, Huangpu Dist., Shanghai',
+    address1: '505 Zhongshan Dong Er Road,',
+    address2: 'Huangpu District, Shanghai',
     detailsTitle: 'Details',
     detailsPara: "We would like to invite you for a small dinner at the Rivera Restaurant to celebrate our wedding. It's an occation for family and friends to come together and have a few laughs, some drinks and good food.",
     directions: 'Directions',
+    rsvpTitle: 'RSVP',
+    rsvpSubtitle: 'Please fill in the form below',
+    rsvpName: 'Name',
+    rsvpQuestion: 'Will you be attending?',
+    rsvpYes: 'Yes',
+    rsvpNo: 'No',
+    rsvpGuestRadio: 'Will you be bringing someone?',
+    rsvpName: 'What is their name?',
+    rsvpSubmit: 'Submit',
+    mainNameError: 'Please enter your name',
+    radioError: 'Please select an option',
+    guestNameError: "Please enter your guest's name",
+    rsvpSent: 'Congrats, your RSVP has been sent! See you soon!',
+    rsvpSent1: 'Congrats!',
+    rsvpSent2: 'Your RSVP has been sent successfully!',
+    rsvpSent3: 'See you soon!',
   }
 }
 
@@ -39,65 +73,114 @@ Page({
     currentPage: 0,
     windowHeight: 0,
     containerHeight: 0,
-    toSect: 'one',
+    toSect: 'three',
     isScroll: false,
-    initalLoad: false,
-    currentLanguage: {},
+    initialLoad: false,
+    currentLanguage: texts.zh,
+    oppositeLang: 'EN',
+    mainName: '',
+    attending: null,
+    guest: null,
+    guestName: '',
+    submitDisabled: true,
+    mainNameError: false,
+    attendingError: false,
+    guestChoiceError: false,
+    guestNameError: false,
+    submitLoading: false,
+    sentRSVP: false,
   },
   onLoad: function () {
     wx.loadFontFace({
       family: 'Custom EN',
-      source: 'url("https://fonts.gstatic.com/s/nanummyeongjo/v15/9Btx3DZF0dXLMZlywRbVRNhxy1LuEGI-gZ_Ll9dMHVruCTvHYAnNT2g.0.woff2")',
+      source: 'url("https://cloud-minapp-29654.cloud.ifanrusercontent.com/1hxBVd1wG2mv9lc6.ttf")',
       complete: console.log
     })
     const sysInfo = wx.getSystemInfoSync();
     const self = this;
     setTimeout(function(){
       self.setData({
-        initalLoad: true,
+        initialLoad: true,
+        sentRSVP: app.globalData.sentRSVP,
       })
-    }, 2000);
-    this.setData({
-      currentLanguage: texts['en'],
-    });
+    }, 5000);
+
+    if (sysInfo.language === 'en') {
+      this.setData({
+        currentLanguage: texts['en'],
+        oppositeLang: '中文',
+      });  
+    }
+
     this.setData({
       barHeight: sysInfo.statusBarHeight,
       windowHeight: sysInfo.windowHeight,
       containerHeight: sysInfo.windowHeight,
     });
   },
-
+  onShareAppMessage: function () {
+    return {
+      title: 'An invitation...',
+      path: 'pages/index/index'
+    }
+  },
+  changeLang: function() {
+    if (this.data.oppositeLang === '中文') {
+      this.setData({
+        oppositeLang: 'EN',
+        currentLanguage: texts.zh,
+      });
+    } else {
+      this.setData({
+        oppositeLang: '中文',
+        currentLanguage: texts.en,
+      });
+    }
+  },
   setInitPos: function (e) {
     this.setData({
       initTouch: e.touches[0].pageY,
     })
   },
-
   scrollTo: function (e) {
-    const wiggleRoom = Math.abs(this.data.initTouch - e.changedTouches[0].pageY) > 20;
+    if (this.data.initialLoad) {
+      const wiggleRoom = Math.abs(this.data.initTouch - e.changedTouches[0].pageY) > 20;
+      const currentPage = this.data.currentPage;
+      this.setData({
+        isScroll: true,
+      })
+      if (wiggleRoom && this.data.initTouch > e.changedTouches[0].pageY) {
+        if (currentPage >= 0 && currentPage < 2) {
+          this.setData({
+            isScroll: true,
+            currentPage: currentPage + 1,
+            toSect: order[currentPage + 1],
+          });
+        }
+      } else if (wiggleRoom && this.data.initTouch < e.changedTouches[0].pageY) {
+        if (currentPage > 0 && currentPage <= 2) {
+          this.setData({
+            currentPage: currentPage - 1,
+            toSect: order[currentPage - 1],
+            isScroll: true,
+          });
+        }
+      } else {
+      }
+      this.setData({
+        isScroll: false,
+      });
+    }
+  },
+  nextSect: function() {
     const currentPage = this.data.currentPage;
-    console.log(currentPage)
     this.setData({
       isScroll: true,
-    })
-    if (wiggleRoom && this.data.initTouch > e.changedTouches[0].pageY) {
-      if (currentPage >= 0 && currentPage < 2) {
-        this.setData({
-          isScroll: true,
-          currentPage: currentPage + 1,
-          toSect: order[currentPage + 1],
-        });
-      }
-    } else if (wiggleRoom && this.data.initTouch < e.changedTouches[0].pageY) {
-      if (currentPage > 0 && currentPage <= 2) {
-        this.setData({
-          currentPage: currentPage - 1,
-          toSect: order[currentPage - 1],
-          isScroll: true,
-        });
-      }
-    } else {
-    }
+    });
+    this.setData({
+      currentPage: currentPage + 1,
+      toSect: order[currentPage + 1],
+    });
     this.setData({
       isScroll: false,
     });
@@ -112,5 +195,118 @@ Page({
       latitude: 31.228675,
       scale: 14,
     })
+  },
+  bindInput: function(e) {
+    if (e.target.dataset.input === 'mainName'){
+      this.setData({
+        mainName: e.detail.value,
+        mainNameError: false,
+      });
+    } else if (e.target.dataset.input === 'guestName') {
+      this.setData({
+        guestName: e.detail.value,
+        guestNameError: false,
+      });
+    }
+  },
+  radioChange: function(e) {
+    if (e.detail.value === 'yes-attend') {
+      this.setData({
+        attending: true,
+        guest: null,
+        attendingError: false,
+      });
+    } else if (e.detail.value === 'no-attend') {
+      this.setData({
+        attending: false,
+        guest: false,
+        attendingError: false,
+      });
+    } else if (e.detail.value === 'yes-guest') {
+      this.setData({
+        guest: true,
+        guestChoiceError: false,
+      });
+    } else if (e.detail.value === 'no-guest') {
+      this.setData({
+        guest: false,
+        guestChoiceError: false,
+        guestNameError: false,
+      });
+    }
+  },
+  submitRSVP: function() {
+    if (this.data.mainName.trim() === '') {
+      this.setData({
+        mainNameError: true,
+      });
+    } else {
+      this.setData({
+        mainNameError: false,
+      });
+    }
+    if (this.data.attending === null) {
+      this.setData({
+        attendingError: true,
+      });
+    } else {
+      this.setData({
+        attendingError: false,
+      });
+    }
+    if (this.data.attending && this.data.guest === null) {
+      this.setData({
+        guestChoiceError: true,
+      })
+    } else {
+      this.setData({
+        guestChoiceError: false,
+      })
+    }
+    if (this.data.guest && this.data.guestName.trim() === '') {
+      this.setData({
+        guestNameError: true,
+      })
+    } else {
+      this.setData({
+        guestNameError: false,
+      })
+    }
+
+    if (!this.data.mainNameError && !this.data.attendingError && !this.data.guestChoiceError && !this.data.guestNameError) {
+      wx.showLoading({
+        mask: true,
+      });
+      let rsvpTable = new wx.BaaS.TableObject('rsvp');
+
+      let newRSVP = rsvpTable.create();
+      const payload = {
+        name: this.data.mainName,
+        attending: this.data.attending,
+        guest: this.data.guest,
+      }
+
+      if (this.data.guest) {
+        payload.guestName = this.data.guestName;
+      }
+      newRSVP.set(payload);
+
+      newRSVP.save().then((res) => {
+        console.log('create res', res);
+        this.setData({
+          sentRSVP: true,
+        });
+        wx.hideLoading();
+      }, (err) => {
+        wx.hideLoading({
+          complete() {
+            wx.showToast({
+              image: 'img/error.png',
+              title: 'Server Error',       
+            });
+          },
+        })
+      });
+    }
   },
 })
